72cd421d5d811688467f0291c9c207228c0aa4f1
Lucas Saliba <lucassaliba10@gmail.com>
2022-11-30 20:48:55 -0300

    👷 Try auth pages

---
+++ src/pages/authentication/componets/sign-in/index.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+import { useDispatch } from "react-redux";
+import { createUserDocumentFromAuth, signInWithGooglePopup, signWithPassword } from "../../../../store/auth/auth";
+import Button from "../../../../components/button";
+
+import { AppDispatch, RootState } from "../../../../store/store";
+
+import './style.scss'
+
+function SignIn() {
+    const defaultFormFields = {
+        email: '',
+        password: '',
+    }
+
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const { email, password } = formFields;
+    const dispatch = useDispatch<AppDispatch>();
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormFields({ ...formFields, [name]: value })
+    }
+
+    const SignWithEmailPassword = (e) => {
+        e.preventDefault();
+        dispatch(signWithPassword({ email, password }))
+    };
+
+    const SignWithGoolge = async (e) => {
+        e.preventDefault();
+        dispatch(signInWithGooglePopup())
+            .then(({ payload }: any) => {
+
+                const { displayName, email, uid, photoURL } = payload;
+
+                const authenticatedUser = {
+                    uid: uid,
+                    email: email,
+                    displayName: displayName,
+                    photoURL: photoURL,
+                    favorites: [],
+                    isLogged: true,
+                };
+
+                dispatch(createUserDocumentFromAuth(authenticatedUser))
+            })
+    }
+
+    return (
+        <form action="" className="sign-in">
+            <h2 className="sign-in__title mb-2">Alredy have an account?</h2>
+            <span>Sign in With your email and password</span>
+
+            <div className="input-box mb-2">
+                <input className="input" type="email" name="email" placeholder="Email" value={email} onChange={handleChange} />
+                <input className="input" type="password" name="password" placeholder="Password" value={password} onChange={handleChange} />
+            </div>
+
+            <div className="sign-in__btns">
+                <Button onClick={(e) => SignWithEmailPassword(e)}>
+                    Sign in
+                </Button>
+
+                <Button onClick={(e) => SignWithGoolge(e)}>
+                    Sign with Google
+                </Button>
+            </div>
+        </form>
+    );
+}
+
+export default SignIn;
---
+++ src/pages/authentication/componets/sign-up/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createUserWithPasswordAndEmail } from "../../../../store/auth/auth";
+import { AppDispatch } from "../../../../store/store";
+
+import './style.scss'
+
+type Props = {};
+
+function SignUp({ }: Props) {
+    const defaultFormFields = {
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const { displayName, email, password, confirmPassword } = formFields;
+    const dispatch = useDispatch<AppDispatch>();
+
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormFields({ ...formFields, [name]: value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) return alert('Password incorrect')
+
+        dispatch(createUserWithPasswordAndEmail({ email, password, displayName }))
+    }
+
+    return (
+        <section className="sign-up mb-2">
+            <h2 className="sign-up__title">Dont't have an account?</h2>
+            <p>Sign up With your email and password</p>
+
+            <form onSubmit={(event) => { handleSubmit(event) }} className='sign-up__form'>
+                <div className="input-box">
+                    <input className="input" type="text" name="displayName" placeholder="Display Name" value={displayName} required onChange={handleChange} />
+                    <input className="input" type="email" name="email" placeholder="Email" value={email} required onChange={handleChange} />
+                    <input className="input" type="password" name="password" placeholder="Password" required value={password} onChange={handleChange} />
+                    <input className="input" type="password" name="confirmPassword" placeholder="confirm password" required value={confirmPassword} onChange={handleChange} />
+                </div>
+
+                <button type="submit">
+                    Sign up
+                </button>
+            </form>
+        </section>
+    );
+}
+
+export default SignUp;
