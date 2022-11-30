c71b0fb53b29f575574932b33d3e70ff65828ae1
Lucas Saliba <lucassaliba10@gmail.com>
2022-11-30 20:42:23 -0300

    ✅ Filters function

---
+++ src/utils/genres-list/index.tsx
@@ -0,0 +1,19 @@
+export const genresList = [
+    { name: 'Action', slug: 'action' },
+    { name: 'Indie', slug: 'indie' },
+    { name: 'Adventure', slug: 'adventure' },
+    { name: 'rpg', slug: 'role-playing-games-rpg' },
+    { name: 'Shooter', slug: 'shooter' },
+    { name: 'Strategy', slug: 'strategy' },
+    { name: 'Casual', slug: 'casual' },
+    { name: 'Simulation', slug: 'simulation' },
+    { name: 'Arcade', slug: 'arcade' },
+    { name: 'Puzzle', slug: 'puzzle' },
+    { name: 'Platformer', slug: 'platformer' },
+    { name: 'Racing', slug: 'racing' },
+    { name: 'Sports', slug: 'sports' },
+    { name: 'Family', slug: 'family' },
+    { name: 'Fighting', slug: 'fighting' },
+    { name: 'Educational', slug: 'educational' },
+    { name: 'Card', slug: 'card' }
+]
